30a7f07b5a59d78a35199c61348982aa738201d4
kuwabaray <yukiya104@gmail.com>
2022-09-08 16:59:31 +0900

    bug fix

--- main.dart.js_2.part.js
+++ main.dart.js_2.part.js
@@ -1,7 +1,8 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
 $__dart_deferred_initializers__.current=function(a,b,c,$){var D={
 arN(){return new D.Dx(null)},
-Dx:function Dx(d){this.a=d},
+Dx:function Dx(d){this.c=0
+this.a=d},
 Ti:function Ti(d){this.a=d}},F,G,A,H,B,C,E
 D=a.updateHolder(c[5],D)
 F=c[16]
@@ -12,10 +13,12 @@ B=c[11]
 C=c[2]
 E=c[10]
 D.Dx.prototype={
-L(d){var x=this,w=null,v="created by Freepik - Flaticon",u="https://www.flaticon.com/authors/freepik",t=F.BD(d),s=t*0.04,r=t*0.025
-return G.agr(A.ay(19,0,0,0),A.bg(w,H.jy(A.a([B.cA(A.bg(w,A.dY("Resource Credits",A.eH(w,w,A.ay(217,0,0,0),w,w,w,w,w,w,w,w,r,w,w,C.ai,w,w,!0,w,w,w,w,w,w,w,w),w,w),w,w,w,w,w,new A.ax(0,0,0,r),w),1),B.cA(x.iL("created by Ilham Fitrotul Hayat - Flaticon","images/mail.png","https://www.flaticon.com/authors/ilham-fitrotul-hayat"),1),B.cA(x.iL("created by Kiranshastry - Flaticon","images/skills.png","https://www.flaticon.com/authors/kiranshastry"),1),B.cA(x.iL("created by Icon8","images/location.png","https://icons8.com"),1),B.cA(x.iL(v,"images/work.png",u),1),B.cA(x.iL(v,"images/education.png",u),1),B.cA(x.iL(v,"images/blog.png",u),1),B.cA(x.iL("created by Smashicons - Flaticon","images/user.png","https://www.flaticon.com/authors/smashicons"),1),B.cA(x.iL("created by Good Ware - Flaticon","images/copyright.png","https://www.flaticon.com/authors/good-ware"),1)],y.e),C.ci,C.a0,C.bb),w,w,w,w,new A.ax(s,s,s,s),w,w))},
-iL(d,e,f){var x=null
-return A.bg(x,A.k6(A.a([B.cA(A.bg(x,A.fK(e,C.a8),x,x,x,0,new A.ax(0,0,0,0),x,0),1),B.cA(A.Y8(!1,!0,A.dY(d,A.eH(x,x,C.cC,x,C.kt,x,x,x,x,x,x,0,x,x,x,x,x,!0,x,x,x,x,x,x,x,x),x,x),x,!0,x,x,x,x,x,x,x,x,x,new D.Ti(f),x,x,x,x),6)],y.e),C.a_,C.a0,C.ac),C.i,x,x,x,x,new A.ax(0,0,0,0),x)}}
+L(d){var x=this,w=null,v="created by Freepik - Flaticon",u="https://www.flaticon.com/authors/freepik",t=x.c=F.BD(d),s=t*0.04
+t*=0.025
+return G.agr(A.ay(19,0,0,0),A.bg(w,H.jy(A.a([B.cA(A.bg(w,A.dY("Resource Credits",A.eH(w,w,A.ay(217,0,0,0),w,w,w,w,w,w,w,w,t,w,w,C.ai,w,w,!0,w,w,w,w,w,w,w,w),w,w),w,w,w,w,w,new A.ax(0,0,0,t),w),1),B.cA(x.iL("created by Ilham Fitrotul Hayat - Flaticon","images/mail.png","https://www.flaticon.com/authors/ilham-fitrotul-hayat"),1),B.cA(x.iL("created by Kiranshastry - Flaticon","images/skills.png","https://www.flaticon.com/authors/kiranshastry"),1),B.cA(x.iL("created by Icon8","images/location.png","https://icons8.com"),1),B.cA(x.iL(v,"images/work.png",u),1),B.cA(x.iL(v,"images/education.png",u),1),B.cA(x.iL(v,"images/blog.png",u),1),B.cA(x.iL("created by Smashicons - Flaticon","images/user.png","https://www.flaticon.com/authors/smashicons"),1),B.cA(x.iL("created by Good Ware - Flaticon","images/copyright.png","https://www.flaticon.com/authors/good-ware"),1)],y.e),C.ci,C.a0,C.bb),w,w,w,w,new A.ax(s,s,s,s),w,w))},
+iL(d,e,f){var x=null,w=this.c,v=w*0.015,u=w*0.02
+w*=0.05
+return A.bg(x,A.k6(A.a([B.cA(A.bg(x,A.fK(e,C.a8),x,x,x,w,new A.ax(0,0,u,0),x,w),1),B.cA(A.Y8(!1,!0,A.dY(d,A.eH(x,x,C.cC,x,C.kt,x,x,x,x,x,x,u,x,x,x,x,x,!0,x,x,x,x,x,x,x,x),x,x),x,!0,x,x,x,x,x,x,x,x,x,new D.Ti(f),x,x,x,x),6)],y.e),C.a_,C.a0,C.ac),C.i,x,x,x,x,new A.ax(u,v,0,v),x)}}
 var z=a.updateTypes([])
 D.Ti.prototype={
 $0(){var x=0,w=A.a3(y.f),v=this,u
@@ -38,4 +41,4 @@ x(D.Dx,A.aL)
 x(D.Ti,A.im)})()
 A.ia(b.typeUniverse,JSON.parse('{"Dx":{"aL":[],"i":[]}}'))
 var y={e:A.S("q<i>"),f:A.S("~")}}
-$__dart_deferred_initializers__["4BWyONKsMEXID/V55bdE8hVt+i0="] = $__dart_deferred_initializers__.current
+$__dart_deferred_initializers__["mbUq2OfKXWgFpuUevQaZH9YGoq4="] = $__dart_deferred_initializers__.current
